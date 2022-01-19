1e7845f2b75e6360baf92ba16dba8adc4d4f5db8
Michael Waldron <mikewaldron5@gmail.com>
2022-01-19 16:16:37 -0500

    commit tree 1cd1872fdbf19893abea7698c3b6d4fa45b7406f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+    const userData ={
+        name: "Steve",
+        email: "Steve@steve.com",
+    };
+
+    const conFigUser = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(userData),
+    };
+
+    fetch("http://localhost:3000/users", conFigUser)
+     .then(function (resp) {
+         return resp.json();
+     })
+     .then(function (object) {
+         console.log(object);
+     })
+     .catch(function (error) {
+         alert("Something went wrong!");
+     });
